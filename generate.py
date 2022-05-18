fc386a3b5b27b21a879a84d9b7c825b6f03f0f94
Matthew Via <via@matthewvia.info>
2022-05-17 20:50:41 -0400

    Do logic in python and not jinja

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
-from cmsis_svd.parser import SVDParser, SVDRegister, SVDField
+from cmsis_svd.parser import SVDParser, SVDRegister, SVDField, SVDPeripheral
 from jinja2 import Environment, FileSystemLoader
+import json
 
 
 
@@ -26,25 +27,52 @@ class Field:
     self.description = field.description
     self.bit_width = field.bit_width
     self.bit_offset = field.bit_offset
-    self.access = field.access
+
+class ReservedSpace:
+  def __init__(self, position, size):
+    self.size = size
+    self.address_offset = position
+    self.name = f"RESERVED_{position}"
+    self.modifier = "volatile const"
 
 class Register:
   def __init__(self, register: SVDRegister):
     self.name = register.name
+    self.size = 1
     self.description = register.description
     self.address_offset = register.address_offset
     self.fields = [Field(f) for f in register.fields]
+    self.access = register.access
+    if self.access == "read-only":
+      self.modifier = "volatile const"
+    else:
+      self.modifier = "volatile"
 
+class Peripheral:
+   def __init__(self, periph: SVDPeripheral):
+     self.name = periph.name
+     self.base_address = periph.base_address
+     self.locations = {}
+     for register in periph.registers:
+       reg = Register(register)
+       if reg.address_offset not in self.locations.keys():
+         self.locations[reg.address_offset] = []
+       self.locations[reg.address_offset].append(reg)
+
+     end_of_last = 0
+     for addr in sorted(self.locations.keys()):
+       delta = addr - end_of_last
+       if delta > 0:
+         self.locations[end_of_last] = [ReservedSpace(end_of_last, delta / 4)]
+       end_of_last = addr + 4
 
 
 ints = enumerate_interrupts(device)
+periphs = [Peripheral(p) for p in device.peripherals]
 env = Environment(loader=FileSystemLoader("templates"))
 vectors_template = env.get_template("vectors.c.j2")
 device_template = env.get_template("device.h.j2")
+validate_template = env.get_template("validate.c.j2")
 open("vectors.c", "w").write(vectors_template.render(interrupts=ints))
-open("device.h", "w").write(device_template.render(interrupts=ints,
-peripherals=device.peripherals))
-
-#for p in device.peripherals:
-#  r = Register(p.registers[0])
-#  print(r.__dict__)
+open("device.h", "w").write(device_template.render(interrupts=ints, peripherals=periphs))
+open("validate.c", "w").write(validate_template.render(peripherals=device.peripherals))
